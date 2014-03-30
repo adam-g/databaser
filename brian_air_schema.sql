e27705e7bd21144e4110fe8b204f2c9e3facbb8c
adagu229 <adagu229@student.liu.se>
2014-03-30 10:56:04 +0200

    commit av brian_air_schema.sql tagning 2

---
+++ brian_air_schema.sql
@@ -0,0 +1,10 @@
+/*
+* Det här är Adams fulkopierade fil med kod från testfile,
+* det är de 6 raderna efter att den här kommentaren tar slut. 
+*/
+
+create table test (id int, name varchar(20));
+
+insert into test values (157, 'kalle');
+
+select * from test;
