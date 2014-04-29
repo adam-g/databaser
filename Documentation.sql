31181de671a2a48de368b2db757b9ca45df955cc
Tobias Genborg <tobias.genborg@gmail.com>
2014-04-29 14:59:59 +0200

    Added documentation/project status file.

---
+++ Documentation.sql
@@ -0,0 +1,74 @@
+/*
+Documentation file
+	Contents:
+		1.1 Format of the status
+		1.2 Status of the exercises 
+ */
+
+/* 1 */
+/* ----------------------------------------------------------------------------------- */
+	/* 1.1 */
+	/* Format:
+		-- The Excersise status is written on this format:
+
+			Excercise #:
+		-- Description: <Description of the excersise>
+		-- Comments: <Comments (e.g. what is left to do, problems )>
+		-- Status: 														[?]
+	*/
+
+/* ----------------------------------------------------------------------------------- */
+
+	/* 1.2 */
+	/*
+	Excercise 1:
+		-- Description: EER modeling
+		-- Status: 	[a-j]												[DONE]
+
+	Excercise 2:
+		-- Description: Translate to tables
+		-- Comments: Do we need to normalise?
+		-- Status: 														[?]
+
+	Excercise 3:
+		-- Description: Set up the database, populate database
+		-- Status: 	[a-b]												[DONE]
+
+	Excercise 4:
+		-- Description: Write procedures
+		-- Comments: Mostly done. Might need to do some error control.
+		-- Status: 	[a] 												[DONE]
+					[b]													[DONE]
+					[c]													[DONE. Questions: NOT DONE]
+					[d]													[DONE. 'Smart' formula: NOT DONE]
+					[e]													[NOT DONE]
+					[Check if the above is enough; are we missing 
+						any necessary queries to complete a booking]	[NOT DONE]
+					
+	Excercise 5:
+		-- Description: Create queries that confirm that a reservation is ready to be paid.
+		-- Status: 	[Do a contact exists?]								[DONE]
+					[Correct # of passengers?]							[NOT DONE]
+
+	Excercise 6:
+		-- Description: Query for show the available seats
+		-- Status: 	[Create query]										[DONE]					
+					[Implemention]										[DONE]
+
+	Excercise 7:
+		-- Description: Calculate price query
+		-- Status: 	[Create query]										[DONE]
+					[Implemention]										[DONE]
+
+	Excercise 8:
+		-- Description: Create search query
+		-- Status: [Create query]										[NOT DONE]
+
+	*/
+
+
+
+/* ----------------------------------------------------------------------------------- */
+
+
+
